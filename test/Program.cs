86b1962778b531944c0dacfda9ee18b06000b7e5
mag1987 <mag987@list.ru>
2018-11-09 21:40:26 +0700

    Revert "переменная b"
    This reverts commit 1c935de323965d54c2585a56ade58f95486d07f4.

--- test/Program.cs
+++ test/Program.cs
@@ -8,8 +8,6 @@ namespace test
         {
             Console.WriteLine("Hello, world!");
             int a = 6;
-            int b = 3;
-            Console.WriteLine(a+b);
             Console.WriteLine("Hello, world again!");
         }
     }
