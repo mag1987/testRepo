778c96706ef65d5445905df2548b5105c3d73c30
mag1987 <44873794+mag1987@users.noreply.github.com>
2018-11-09 21:50:58 +0700

    Update Program.cs

--- test/Program.cs
+++ test/Program.cs
@@ -7,7 +7,7 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-            int a = 6;
+            int a = 7;
             int b = 3;
             Console.WriteLine(a+b);
             Console.WriteLine("Hello, world again!");
