0f69709dd719e9a9ff8e9bfa7ed4fea1aeb0af38
mag1987 <mag987@list.ru>
2018-11-09 22:05:08 +0700

    возврат к переменной b

--- test/Program.cs
+++ test/Program.cs
@@ -7,7 +7,13 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+<<<<<<< HEAD
             int a = 6;
+=======
+            int a = 7;
+            int b = 3;
+            Console.WriteLine(a+b);
+>>>>>>> 778c96706ef65d5445905df2548b5105c3d73c30
             Console.WriteLine("Hello, world again!");
         }
     }
