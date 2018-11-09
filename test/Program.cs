1c935de323965d54c2585a56ade58f95486d07f4
mag1987 <mag987@list.ru>
2018-11-09 20:15:04 +0700

    переменная b

--- test/Program.cs
+++ test/Program.cs
@@ -8,6 +8,8 @@ namespace test
         {
             Console.WriteLine("Hello, world!");
             int a = 6;
+            int b = 3;
+            Console.WriteLine(a+b);
             Console.WriteLine("Hello, world again!");
         }
     }
