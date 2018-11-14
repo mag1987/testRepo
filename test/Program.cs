c43e3ccf61ecde526c6c3eacc6cc0fef170ccc5f
mag1987 <44873794+mag1987@users.noreply.github.com>
2018-11-14 15:21:03 +0700

    Branch from old commit

--- test/Program.cs
+++ test/Program.cs
@@ -8,7 +8,8 @@ namespace test
         {
             Console.WriteLine("Hello, world!");
             int a = 6;
-            int b = 3;
+            int b = 56;
+            string newBranch;
             Console.WriteLine(a+b);
             Console.WriteLine("Hello, world again!");
         }
