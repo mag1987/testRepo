b551a25f6968489298824a3583d15c982b79415c
mag1987 <44873794+mag1987@users.noreply.github.com>
2018-11-17 16:19:51 +0700

    cleared

--- test/Program.cs
+++ test/Program.cs
@@ -7,15 +7,10 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-<<<<<<< HEAD
-<<<<<<< HEAD
             int a = 6;
-string someupdate2;
->>>>>>> 778c96706ef65d5445905df2548b5105c3d73c30
-            int a = 7;
+            string someupdate2;
             int b = 3;
             Console.WriteLine(a+b);
->>>>>>> 778c96706ef65d5445905df2548b5105c3d73c30
             Console.WriteLine("Hello, world again!");
         }
     }
